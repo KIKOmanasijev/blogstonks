00e1a8aefb4863b3a56a2e75c26c2385111af8ff
KIKOmanasijev <hristijan.manasijev@nteams.com>
2025-09-27 11:51:14 +0200

    wip

--- routes/console.php
+++ routes/console.php
@@ -4,8 +4,16 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-// Schedule blog scraping every 3 minutes
-Schedule::command('scrape:blogs')->everyThreeMinutes();
+// Schedule blog scraping every 3 minutes on weekdays (Mon-Fri)
+Schedule::command('scrape:blogs')
+    ->everyThreeMinutes()
+    ->weekdays()
+    ->between('09:00', '17:00'); // Only during business hours
+
+// Schedule blog scraping every 3 hours on weekends
+Schedule::command('scrape:blogs')
+    ->everyThreeHours()
+    ->weekends();
 
 // Schedule stock price fetching every 30 seconds
 Schedule::command('stocks:fetch')->everyThirtySeconds();
